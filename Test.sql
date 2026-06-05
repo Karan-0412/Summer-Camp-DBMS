88d0853e9db1a0d3a86915957da70701b96e9191
Karan <karan962575@gmail.com>
2026-06-05 12:04:04 +0530

    Update Test.sql

--- Test.sql
+++ Test.sql
@@ -1,19 +1,4 @@
-CREATE TABLE transactions
-(
-transaction_id INT PRIMARY KEY,
-merchant_id INT,
-credit_card_id INT,
-amount INT,
-transaction_timestamp TIMESTAMP
-);
-INSERT INTO transactions
-VALUES
-(1, 101, 1, 100, '2022-09-25 12:00:00'),
-(2, 101, 1, 100, '2022-09-25 12:08:00'),
-(3, 101, 1, 100, '2022-09-25 12:28:00'),
-(4, 102, 2, 300, '2022-09-25 12:00:00'),
-(6, 102, 2, 400, '2022-09-25 14:00:00') ;
-
+----Soultion - 1 -----
 
 select count(t2.transaction_id) as payment_count
 from transactions t1 
@@ -23,22 +8,7 @@ and t2.amount = t1.amount
 and t2.transaction_timestamp > t1.transaction_timestamp  
 and t2.transaction_timestamp <= t1.transaction_timestamp + INTERVAL '10 minutes' ;
 
-
-CREATE TABLE user_actions
-(
-user_id INT,
-event_id INT,
-event_type VARCHAR(20),
-event_date TIMESTAMP
-);
-INSERT INTO user_actions
-VALUES
-(445, 7765, 'sign-in', '2022-06-05 12:00:00'),
-(742, 6458, 'sign-in', '2022-06-10 12:00:00'),
-(648, 3124, 'like', '2022-06-18 12:00:00'),
-(445, 3634, 'like', '2022-07-05 12:00:00'),
-(742, 1374, 'comment', '2022-07-15 12:00:00'),
-(999, 5555, 'sign-in', '2022-07-20 12:00:00');
+---- Solution-2 ------
 
 select 7 as months, count(distinct u1.user_id) 
 from user_actions u1
@@ -48,23 +18,7 @@ where Extract(month from u1.event_date) = 6 and
 Extract(month from u2.event_date) = 7 and
 Extract(year from u2.event_date) = 2022 ;
 
-CREATE TABLE Submissions (
-sub_id INT,
-parent_id INT
-);
-INSERT INTO Submissions (sub_id, parent_id) VALUES
-(1, NULL),
-(2, NULL),
-(1, NULL),
-(12, NULL),
-(3, 1),
-(5, 2),
-(3, 1),
-(4, 1),
-(9, 1),
-(10, 2),
-(6, 7);
-
+----- Solution-3 -------
 
 Select sub_id as post_id, 
 (Select count(distinct s2.sub_id)
